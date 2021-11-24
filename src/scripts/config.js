461ab5766872fc92c1820f79751c22bbd0d5876c
mojotron <igormalj@gmail.com>
2021-11-24 20:55:33 +0100

    Prepare helper functions for minimax - find available moves - find best move - remove magic data, set variables to config file

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,2 +1,5 @@
 export const ROW_NUM = 6;
 export const COL_NUM = 7;
+export const TOKEN_EMPTY_CELL = '--';
+export const TOKEN_PLAYER_1 = '😸';
+export const TOKEN_AI = '🤖';
--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,9 +1,15 @@
 import '../styles/reset.css';
 import '../styles/main.css';
-import { ROW_NUM, COL_NUM } from './config';
+import {
+  ROW_NUM,
+  COL_NUM,
+  TOKEN_EMPTY_CELL,
+  TOKEN_PLAYER_1,
+  TOKEN_AI,
+} from './config';
 import checkWinCondition from './check-win-condition';
 
-const createBoard = function (row, col, cellValue = 0) {
+const createBoard = function (row, col, cellValue = TOKEN_EMPTY_CELL) {
   return Array.from({ length: row }, () =>
     Array.from({ length: col }, () => cellValue)
   );
@@ -11,17 +17,19 @@ const createBoard = function (row, col, cellValue = 0) {
 
 const displayBoard = function (board) {
   console.log('--Connect four board--');
-  board.forEach(row => console.log(row));
+  const reversed = JSON.parse(JSON.stringify(board));
+  reversed.reverse();
+  reversed.forEach(row => console.log(row));
   console.log('--Chose row number--');
   console.log(Array.from({ length: board.length + 1 }, (_, i) => i));
 };
 //
 const findEmptyColumnSlot = function (board, colIndex) {
   // check if top of the column empty
-  if (board[board.length - 1][colIndex] !== 0) return -1;
+  if (board[board.length - 1][colIndex] !== TOKEN_EMPTY_CELL) return -1;
   // find first empty slot form the bottom of the column
   for (let i = 0; i < board.length; i += 1) {
-    if (board[i][colIndex] === 0) return i;
+    if (board[i][colIndex] === TOKEN_EMPTY_CELL) return i;
   }
   return -1;
 };
@@ -43,12 +51,57 @@ const inputToken = function (board, colIndex, tokenSign) {
   checkWinCondition(board, rowIndex, colIndex, tokenSign);
 };
 
+// AI
+// const getNextFreeMoves = function (board) {
+//   const freeSpots = [];
+//   // check columns for free spots
+//   for (let i = 0; i < COL_NUM; i += 1) {
+//     for (let j = 0; j < ROW_NUM; j += 1) {
+//       if (board[j][i] === 0) {
+//         freeSpots.push([j, i]);
+//         break;
+//       }
+//     }
+//   }
+//   if (!freeSpots.length) return false;
+//   return freeSpots;
+// };
+
+// const evaluateMove = function () {};
+
+// const minimax = function (position, depth, maximizer) {
+//   // break condition
+//   if (maximizer) {
+//     const bestMove = -Infinity;
+//   } else {
+//     const bestMove = Infinity;
+//     // loop
+//   }
+// };
+
+// const findBestMove = function (boardState) {
+//   const bestValue = -Infinity;
+//   let bestMove;
+//   const freeMoves = getNextFreeMoves(boardState);
+//   for (const move of freeMoves) {
+//     boardState[move[0]][move[1]] = TOKEN_AI;
+//     const currentValue = minimax(boardState, 3, false);
+//     boardState[move[0]][move[1]] = 0;
+//     if (currentValue > bestValue) {
+//       bestValue = currentValue;
+//       bestMove = move;
+//     }
+//   }
+//   return bestMove;
+// };
+
 // Basic game logic
-const board = createBoard(ROW_NUM, COL_NUM, 0);
+const board = createBoard(ROW_NUM, COL_NUM);
 displayBoard(board);
+// findBestMove(board);
 let i = 0;
 while (i < 42) {
-  const token = i % 2 === 0 ? 1 : 2;
+  const token = i % 2 === 0 ? TOKEN_PLAYER_1 : TOKEN_AI;
   const userInput = Number(prompt('Enter column'));
   console.clear();
   inputToken(board, userInput, token);
